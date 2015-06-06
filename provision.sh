3910ee76ca239c719b4384c1620ca29f0e366dc1
anders <anders.lindmark@gmail.com>
2015-06-06 15:13:46 +0200

    Forgot to include SyncSim

--- provision.sh
+++ provision.sh
@@ -7,9 +7,12 @@ mkdir /software
 tar xjvf /vagrant/mips-sde.tar-06.61.bz2 -C /software/
 ln -s /software/mips-sde/06.61 /software/mips-sde/current
 
+# SyncSim
+cp /vagrant/SyncSim.jar ~vagrant/
+
 # course files
 tar xjvf /vagrant/d0013e_dist.tar.bz2 -C ~vagrant/
 tar xjvf /vagrant/coursepage.tar.bz2 -C ~vagrant/
 
 echo "export PATH='/software/mips-sde/current/bin:$PATH'" >> ~vagrant/.bashrc
-
+echo "alias syncsim='java -jar ~/SyncSim.jar'" >> ~vagrant/.bashrc
