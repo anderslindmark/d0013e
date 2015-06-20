d3fd7c38dcff8cdc8594602cefc9a66dc00f38fb
Anders Lindmark <anders.lindmark@gmail.com>
2015-06-20 17:52:00 +0200

    Cleanup

--- provision.sh
+++ provision.sh
@@ -1,24 +1,26 @@
-# packages
+echo "Installing packages"
 apt-get update
 apt-get install -y tmux vim nginx make openjdk-7-jre
 
-# mips-sde
+echo "Installing mips-sde"
 mkdir /software
 tar xjf /vagrant/provision/mips-sde.tar-06.61.bz2 -C /software/
 ln -s /software/mips-sde/06.61 /software/mips-sde/current
+echo "export PATH='/software/mips-sde/current/bin:$PATH'" >> ~vagrant/.bashrc
 
-# SyncSim
+echo "Copying SyncSim"
 cp /vagrant/provision/SyncSim.jar ~vagrant/
+echo "alias syncsim='java -jar ~/SyncSim.jar'" >> ~vagrant/.bashrc
 
-# course files
+echo "Copying course page and configuring webserver"
 tar xjf /vagrant/provision/d0013e_dist.tar.bz2 -C ~vagrant/
 tar xjf /vagrant/provision/coursepage.tar.bz2 -C ~vagrant/
 cp /vagrant/provision/coursepage_nginx_conf /etc/nginx/sites-enabled/default
 /etc/init.d/nginx restart
 
-echo "export PATH='/software/mips-sde/current/bin:$PATH'" >> ~vagrant/.bashrc
-echo "alias syncsim='java -jar ~/SyncSim.jar'" >> ~vagrant/.bashrc
 
-
-echo "Use 'vagrant ssh -- -Y' to ssh to the box with X-forwarding"
-echo "Course webpage is available at http://localhost:8123"
+echo "Installation done"
+echo "To start syncsim:"
+echo "  * vagrant ssh -- -Y"
+echo "  * java -jar ~/SyncSim.jar"
+echo "The course webpage mirror is available at http://localhost:8123"
