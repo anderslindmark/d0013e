13351ac6474c9b537d511f29e86cc954f1f1a2c8
Anders Lindmark <anders.lindmark@gmail.com>
2015-06-20 14:09:24 +0200

    Serve coursepage from the virtual machine

--- provision.sh
+++ provision.sh
@@ -1,18 +1,24 @@
 # packages
 apt-get update
-apt-get install -y tmux make openjdk-7-jre
+apt-get install -y tmux vim nginx make openjdk-7-jre
 
 # mips-sde
 mkdir /software
-tar xjvf /vagrant/mips-sde.tar-06.61.bz2 -C /software/
+tar xjf /vagrant/mips-sde.tar-06.61.bz2 -C /software/
 ln -s /software/mips-sde/06.61 /software/mips-sde/current
 
 # SyncSim
 cp /vagrant/SyncSim.jar ~vagrant/
 
 # course files
-tar xjvf /vagrant/d0013e_dist.tar.bz2 -C ~vagrant/
-tar xjvf /vagrant/coursepage.tar.bz2 -C ~vagrant/
+tar xjf /vagrant/d0013e_dist.tar.bz2 -C ~vagrant/
+tar xjf /vagrant/coursepage.tar.bz2 -C ~vagrant/
+cp /vagrant/coursepage_nginx_conf /etc/nginx/sites-enabled/default
+/etc/init.d/nginx restart
 
 echo "export PATH='/software/mips-sde/current/bin:$PATH'" >> ~vagrant/.bashrc
 echo "alias syncsim='java -jar ~/SyncSim.jar'" >> ~vagrant/.bashrc
+
+
+echo "Use 'vagrant ssh -- -Y' to ssh to the box with X-forwarding"
+echo "Course webpage is available at http://localhost:8123"
